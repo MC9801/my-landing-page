ffc367e83eaccbf3ce117e113f969dd81abf62be
Maria del Carmen Amarilla Cabrera <110703272+MC9801@users.noreply.github.com>
2025-11-06 18:24:36 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    // Next.js 15 puede tener configuraciones experimentales
-  },
-}
-
-module.exports = nextConfig
